6a5ffb76f9e5eb4ab509447360ed2f77a4593c25
Lucas Helal <lucasghelal@gmail.com>
2016-05-11 13:37:08 -0300

    publish and delete

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
